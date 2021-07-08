b08174dab748e5782d367c8c375d51b39227b298
nao machida <xo224324xo@icloud.com>
2021-07-08 15:16:30 +0900

    画像が表示されるよう修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root :to => 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
-  resources :pictures
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
 end
